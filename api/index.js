5ca3ba4dc15aac717bea62afd756a45b868f62e6
Kali <kaleandra_hirata@hotmail.com>
2022-11-27 21:22:16 -0300

    vercel configuration

--- api/index.js
+++ api/index.js
@@ -2,6 +2,18 @@ const app = require('express')();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+  
+  app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
+
 app.use("/pethouse", routes);
 
 module.exports = app;
